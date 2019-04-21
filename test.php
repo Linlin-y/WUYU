c9e1a1fac8824450b20b7d7be9181e62a9843b6f
Linlin-y <49829596+Linlin-y@users.noreply.github.com>
2019-04-21 09:05:57 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,2 +1,14 @@
 <?php
- ?>
+$Zahl1="1000000000";$Zahle="31536000";$zahl1="977616000";
+$Zahl2="22384000";  $Zahld="2592000"; $zahl2="20736000";
+$Zahl3="1684000";   $Zahlc="86400";   $zahl3="1641600";
+$Zahl4="42400";     $Zahlb="3600";    $zahl4="39600";
+$Zahl5="2800";      $Zahla="60";      $zahl5="2760";
+$ergebnis6= $Zahl5 - $zahl5;
+$ergebnis5= $zahl5 / $Zahla;
+$ergebnis4= $zahl4 / $Zahlb;
+$ergebnis3= $zahl3 / $Zahlc;
+$ergebnis2= $zahl2 / $Zahld;
+$ergebnis1= $zahl1 / $Zahle;
+echo"That is $ergebnis1 years $ergebnis2 months $ergebnis3 days $ergebnis4 hours $ergebnis5 minutes $ergebnis6 secounds.";
+?>
